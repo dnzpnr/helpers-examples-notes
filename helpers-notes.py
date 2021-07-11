7ed172a262a98aa2c5514cf889158d4b980f1317
dnzpnr <dnz1pnr@gmail.com>
2021-07-11 22:17:51 +0300

    changed helpers

--- helpers-notes.py
+++ helpers-notes.py
@@ -89,6 +89,7 @@
 # 12-12 CatBoost
 ''' 13 TUM MODELLERIN KARSILASTIRILMASI '''
 # 13 TUM MODELLERIN KARSILASTIRILMASI
+# Tum modellerin ogrendiklerini birlestirip yeni bir skor elde etmek
 ''' ///// UNSUPERVISED LEARNING \\\\\ '''
 ''' 14 UNSUPERVISED LEARNING '''
 # 14-1 K-means ile kendi icinde homojen ama birbirinden farkli kumeler olusturma
@@ -1128,6 +1129,46 @@ sns.barplot(x='Accuracy', y='Modeller', data=sonuclar, color="r")
 plt.xlabel('Accuracy %')
 plt.title('Modellerin Doğruluk Oranları');
 #%%
+#13-2 Tum modellerin ogrendiklerini birlestirip yeni bir skor elde etmek
+class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, base_models, meta_model, n_folds=5):
+        self.base_models = base_models
+        self.meta_model = meta_model
+        self.n_folds = n_folds
+
+    # We again fit the data on clones of the original models
+    def fit(self, X, y):
+        self.base_models_ = [list() for x in self.base_models]
+        self.meta_model_ = clone(self.meta_model)
+        kfold = KFold(n_splits=self.n_folds, shuffle=True, random_state=156)
+
+        # Train cloned base models then create out-of-fold predictions
+        # that are needed to train the cloned meta-model
+        out_of_fold_predictions = np.zeros((X.shape[0], len(self.base_models)))
+        for i, model in enumerate(self.base_models):
+            for train_index, holdout_index in kfold.split(X, y):
+                instance = clone(model)
+                self.base_models_[i].append(instance)
+                instance.fit(X[train_index], y[train_index])
+                y_pred = instance.predict(X[holdout_index])
+                out_of_fold_predictions[holdout_index, i] = y_pred
+
+        # Now train the cloned  meta-model using the out-of-fold predictions as new feature
+        self.meta_model_.fit(out_of_fold_predictions, y)
+        return self
+    #Do the predictions of all base models on the test data and use the averaged predictions as
+    #meta-features for the final prediction which is done by the meta-model
+    def predict(self, X):
+        meta_features = np.column_stack([
+            np.column_stack([model.predict(X) for model in base_models]).mean(axis=1)
+            for base_models in self.base_models_ ])
+        return self.meta_model_.predict(meta_features)
+        
+stacked_averaged_models = StackingAveragedModels(base_models = (ENet, GBoost, KRR),
+                                                 meta_model = lasso)
+
+score = rmsle_cv(stacked_averaged_models)
+print("Stacking Averaged models score: {:.4f}".format(score.mean()))
 #%%
 # 14- UNSUPERVISED LEARNING (GOZETIMSIZ OGRENME)
 # 14-1 K-means ile kendi icinde homojen ama birbirinden farkli kumeler olusturmaya calisilir. Hiyerarsik olmayan kumeleme yontemidir. Rastgele noktalar belirlenir ve bu noktalara en yakin noktalar kumlenmis olur
